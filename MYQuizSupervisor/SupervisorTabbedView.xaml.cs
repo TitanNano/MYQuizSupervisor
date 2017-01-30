37462932c1c535c70bc6dff311a98cbecb5c0a1d
mike <michael_tran@web.de>
2017-01-30 15:33:40 +0100

    #13 - Merge View: dummy class added

--- MYQuizSupervisor/SupervisorTabbedView.xaml.cs
+++ MYQuizSupervisor/SupervisorTabbedView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,19 @@ namespace MYQuizSupervisor
         public SupervisorTabbedView()
         {
             InitializeComponent();
+
+            var list = new ObservableCollection<ListItem>();
+
+            list.Add(new ListItem { Title = "Item 1" });
+            list.Add(new ListItem { Title = "Item 2" });
+
+            repeater.ItemsSource = list;
+            autoComplete.Suggestions = list;
         }
+    }
+
+    public class ListItem
+    {
+        public string Title { get; set; }
     }
 }
