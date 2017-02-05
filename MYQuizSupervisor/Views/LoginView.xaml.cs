2132b22273cb607728888d4168052ec5226dce1b
Jovan Gerodetti <TitanNano@users.noreply.github.com>
2017-02-05 11:22:55 +0100

    #2 - Create layout for password view

---
+++ MYQuizSupervisor/Views/LoginView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MYQuizSupervisor
+{
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+
+            btn_login_weiter.HeightRequest = btn_login_weiter.Height;
+        }
+    }
+}
